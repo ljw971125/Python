6bd23b452826c789e271dcf6aac93c9859fdd9fd
Lee <ljw971125@naver.com>
2023-02-16 12:27:41 +0900

    파이썬의 모듈(외부)

---
+++ 20230216/test.py
@@ -0,0 +1,13 @@
+print("이 파일은  모듈 생성하기 예제입니다.")
+
+def add(num1,num2) :
+    return num1+num2
+
+def sub(num1,num2) :
+    return num1-num2
+
+pi = 3.14
+
+print(__name__)
+if(__name__ == "__main__"):
+    print("이 곳은 메인일때만 출력됩니다.")
