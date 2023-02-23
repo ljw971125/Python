a37488d92e47d17642b40d5734c63f49769d07ce
ljw971125 <ljw971125@naver.com>
2023-02-23 10:45:53 +0900

    프로젝트

--- Project/ProjectMain.py
+++ Project/ProjectMain.py
@@ -7,18 +7,17 @@ while(True):
     print('*'*20)
     print("1) 3일간 최대 많이 나온 검색어 상위 20") #완
     print("2) ") 
-    print("3) 검색어 순위 워드 클라우드") #진행중
+    print("3) 검색어 순위 워드 클라우드") #완
     print("4) 검색어 순위 막대 그래프") # 완
-    print("5) 상품 검색 후 브랜드 워드클라우드") #진행중
+    print("5) 상품 검색 후 브랜드 워드클라우드") #완
     print("6) 상품 검색 후 브랜드 원 그래프") #완
     print("7) ")
     print("8) ")
     print("9) ")
     print("0) 종료")
+    
     in_num=int(input("보고싶은 메뉴의 번호를 입력하세요.\n"))
 
-
-    
     if(in_num==1):
         search_top(file_to_counter())
 
@@ -26,30 +25,36 @@ while(True):
         raise Exception('구현 미완성')
 
     elif(in_num==3):
-        # 검색어 순위 워드 클라우드
-        raise Exception('워드 클라우드 미완성')
+        mk_wordcloud(file_to_counter())
 
     elif(in_num==4):
             # 검색어 순위 막대 그래프
         show_bar(file_to_counter())
+        #break
         
 
     elif(in_num==5):
-            # 상품 검색 후 브랜드 리스트 출력
-            # 함수
+        s=input("상품 입력 : \n")
+        mk_wordcloud(search_brand(s))
         print()
         
 
     elif(in_num==6):
         # 상품 검색 후 원 그래프
+        li3=["y","yes","Y","YES","Yes","예","네"]
+        li4=["n","no","N","NO","No","아니오"]
         brand_circle()
+
         print('이미지 저장?')
-        s=input('y/n')
-        if(s=='y' or s=='Y'):
-            raise Exception('저장 기능 미완성')
-            
-        else:
-            raise Exception('그냥 미완성')
+        while(True):
+            s=input('y/n')
+            if(s in li3):
+                print("저장 되었습니다.")
+                break
+            elif(s in li4):
+                break
+            else:
+                print("잘못된 입력입니다.")
         
         
     elif(in_num==7):
@@ -66,7 +71,10 @@ while(True):
     elif(in_num==0):
         print('프로그램을 종료합니다.')
         break
+       
 
     else:
         print("올바르지 않은 입력입니다.")
+# %%
+
 # %%
--- Project/project.py
+++ Project/project.py
@@ -13,41 +13,12 @@ from collections import Counter #카운트
 from urllib.request import urlopen,Request
 import matplotlib.pyplot as plt
 import platform
-
-# In[1]:
-# 3일간 최대 많이 나온 검색어 상위 20
-def search_top(cnt):
-    import operator
-    string_list=list(cnt.keys()) # 읽어온 카운터의 키값(상품명)을 리스트로 저장
-    int_list=list(cnt.values()) # 읽어온 카운터의 value값(빈도수)를 리스트로 저장
-
-    dic = { x:y for x,y in zip(string_list,int_list) } # 두 개의 리스트를 딕셔너리화 (중복제거 x)
-    sorted_by_value = sorted(dic.items(), key=operator.itemgetter(1), reverse=True) # value(빈도수)값으로 내림차순 정렬
-    for i in range(0,20,2):
-        print("%10s \t %10s\n"%(sorted_by_value[i][0],sorted_by_value[i+1][0]))
+from wordcloud import WordCloud
+import numpy as np
+from PIL import Image
+import operator
 
 
-# In[2]:
-# 저장된 파일을 바탕으로 막대그래프
-def show_bar(counter):
-    if platform.system() == 'Darwin': #맥
-        plt.rc('font', family='AppleGothic') 
-    elif platform.system() == 'Windows': #윈도우
-        plt.rc('font', family='Malgun Gothic') 
-    elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-
-        plt.rc('font', family='Malgun Gothic') 
-    plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
-    
-    labels=list(counter.keys())
-    values=list(counter.values())
-    plt.figure(figsize=(20,10))
-    plt.title("3일간의 검색 빈도수")
-    plt.xlabel("검색어")
-    plt.ylabel("빈도수")
-    # for i in range(20):
-    #     plt.bar(labels[i],values[i])
-    plt.bar(labels[:20],values[:20])
 
 def file_to_counter():  
     f=open('save2_data.txt','r',encoding='utf-8') # 축적된 파일 열기
@@ -92,19 +63,50 @@ def get_ranklist():
     return li2
 
 
+# In[1]:
+def search_top(cnt):
+    string_list=list(cnt.keys()) # 읽어온 카운터의 키값(상품명)을 리스트로 저장
+    int_list=list(cnt.values()) # 읽어온 카운터의 value값(빈도수)를 리스트로 저장
 
+    dic = { x:y for x,y in zip(string_list,int_list) } # 두 개의 리스트를 딕셔너리화 (중복제거 x)
+    sorted_by_value = sorted(dic.items(), key=operator.itemgetter(1), reverse=True) # value(빈도수)값으로 내림차순 정렬
+    for i in range(0,20,2):
+        print("%10s \t %10s\n"%(sorted_by_value[i][0],sorted_by_value[i+1][0]))
 
 # 저장된 파일을 바탕으로 워드클라우드
 
 
+# In[1]:
 
+
+# 저장된 파일을 바탕으로 막대그래프
+def show_bar(counter):
+    if platform.system() == 'Darwin': #맥
+        plt.rc('font', family='AppleGothic') 
+    elif platform.system() == 'Windows': #윈도우
+        plt.rc('font', family='Malgun Gothic') 
+    elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+
+        plt.rc('font', family='Malgun Gothic') 
+    plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+    
+    labels=list(counter.keys())
+    values=list(counter.values())
+    plt.figure(figsize=(20,10))
+    plt.title("3일간의 검색 빈도수")
+    plt.xlabel("검색어")
+    plt.ylabel("빈도수")
+    # for i in range(20):
+    #     plt.bar(labels[i],values[i])
+    plt.bar(labels[:20],values[:20])
+    plt.show()
 # In[5]:
 
 
 # 2 키워드별 판매순으로 브랜드 카운팅
 # 상품 검색후 브랜드 카운팅
-def search_brand():    
-    input_product = input('상품명을 입력해주세요.') # 상품명 입력
+def search_brand(item):    
+    #input_product = input('상품명을 입력해주세요.') # 상품명 입력
     
     driver=webdriver.Chrome("C:\chromedriver\chromedriver.exe") #크롬드라이버
     driver.get("https://www.musinsa.com/app/") 
@@ -112,7 +114,7 @@ def search_brand():
     # 크롬 드라이버 동작 부분
     driver.find_element(By.XPATH,'//*[@id="search_query"]').click()
     sleep(0.1)
-    driver.find_element(By.XPATH,'//*[@id="search_query"]').send_keys(input_product)
+    driver.find_element(By.XPATH,'//*[@id="search_query"]').send_keys(item)
     sleep(0.1)
     driver.find_element(By.XPATH,'//*[@id="search_button"]').click()
     sleep(0.1)
@@ -159,45 +161,75 @@ def search_brand():
 
 # 브랜드 파일 원그래프
 def brand_circle():
-        if platform.system() == 'Darwin': #맥
-                plt.rc('font', family='AppleGothic') 
-        elif platform.system() == 'Windows': #윈도우
-                plt.rc('font', family='Malgun Gothic') 
-        elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+    if platform.system() == 'Darwin': #맥
+            plt.rc('font', family='AppleGothic') 
+    elif platform.system() == 'Windows': #윈도우
+            plt.rc('font', family='Malgun Gothic') 
+    elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+        plt.rc('font', family='Malgun Gothic') 
+        plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
 
-                plt.rc('font', family='Malgun Gothic') 
-                plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+    counter = search_brand()
 
-        counter = search_brand()
+    labels=list(counter.keys())
+    values=list(counter.values())
 
-        labels=list(counter.keys())
-        values=list(counter.values())
-
-        high_value = []
-        high_label=[]
+    high_value = []
+    high_label=[]
         
-        for i in range(len(labels)):
-                if(values[i] >= 20):
-                        high_value.append(values[i])
-                        high_label.append(labels[i])
+    for i in range(len(labels)):
+        if(values[i] >= 20):
+            high_value.append(values[i])
+            high_label.append(labels[i])
         
-        explode_max = 0
-        explode_value = []
+    explode_max = 0
+    explode_value = []
 
-        for i in range(len(high_value)):
-                if high_value[i] > explode_max:
-                        explode_max = high_value[i]
+    for i in range(len(high_value)):
+        if high_value[i] > explode_max:
+            explode_max = high_value[i]
         
-        for i in range(len(high_value)):
-                if high_value[i] == explode_max:
-                        explode_value.append(0.1)
-                else:
-                        explode_value.append(0)
+    for i in range(len(high_value)):
+        if high_value[i] == explode_max:
+            explode_value.append(0.1)
+        else:
+            explode_value.append(0)
         
       
-        plt.pie(high_value, labels=high_label, explode=explode_value, autopct='%.2f')
-        plt.show
+    plt.pie(high_value, labels=high_label, explode=explode_value, autopct='%.2f')
+    plt.show
+    # 워드클라우드 공통 함수화
+def mk_wordcloud(func):
 
+
+    labels=list(func.keys())
+    values=list(func.values())
+    high_value2 = [] 
+    high_label2 = []
+
+    for i in range(len(labels)):
+        if(values[i] >= 5):
+            high_value2.append(values[i])
+            high_label2.append(labels[i])
+
+    high_dic = dict(zip(high_label2,high_value2))
+
+    t_mask = np.array(Image.open('t5_2.jpg'))
+
+    fontpath='C:\\Windows\\Fonts\\NGULIM.TTF'
+
+    wc = WordCloud(
+        background_color='white',
+        relative_scaling=0.2, 
+        mask=t_mask, stopwords=['counter'], 
+        colormap ="Greens",
+        font_path = fontpath
+    ).generate(str(high_dic))
+
+    plt.figure(figsize=(8,8))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
 
 # In[ ]:
 
